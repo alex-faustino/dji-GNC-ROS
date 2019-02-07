89832cc310afc3c059de70c5e6466e9912f5e744
Alex Faustino <alex.b.faustino@gmail.com>
2019-02-07 15:48:17 -0600

    working orbit simulation

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/matrice/dji-GNC-ROS/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/matrice/dji-GNC-ROS/devel;/opt/ros/kinetic".split(';') if "/home/matrice/dji-GNC-ROS/devel;/opt/ros/kinetic" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,10 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/matrice/dji-GNC-ROS/devel:/opt/ros/kinetic"
-export LD_LIBRARY_PATH="/home/matrice/dji-GNC-ROS/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
-export PKG_CONFIG_PATH="/home/matrice/dji-GNC-ROS/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/matrice/dji-GNC-ROS/build"
-export PYTHONPATH="/home/matrice/dji-GNC-ROS/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/matrice/dji-GNC-ROS/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/matrice/dji-GNC-ROS/src:/opt/ros/kinetic/share"
